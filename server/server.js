86e2ccf171b8bdf31242deefbd482a813509e037
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-18 21:05:46 +0800

    Add dotenv for server port customize

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js"
+import dotenv from "dotenv"
+dotenv.config()
 
-const port = 8080
+const port = process.env.SERVER_PORT || 8080
 
 app.listen(port, () => {
   console.log(`Server start up at ${port}`)
