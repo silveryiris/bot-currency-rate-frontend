037d4a528f1039318fb4b9fb98ed1a593d7c664c
Wen Wei , Chang <silveryiris@gmail.com>
2019-07-28 22:50:33 +0800

    Imporve raw data display & json download location

--- client/components/CurrencyRate/RawRate.vue
+++ client/components/CurrencyRate/RawRate.vue
@@ -1,13 +1,40 @@
 <template lang="pug">
 .currency-rate-raw
-  code.currency-rate-raw__list(v-for="x in rates") {{ x }}
   a#download-rate-json.currency-rate-raw__download(title="Download JSON file" @click="assignFileName")
     IconCloudDownload.icon-svg.icon-svg--large
     span json
+  pre.currency-rate-raw__list(v-for="x in rates")
+    |    {
+    |      "currency": "{{x.currency}}"" ,
+    |      "buying": {
+    |        "cash": "{{x.buying.cash}}",
+    |        "spot": "{{x.buying.spot}}",
+    |        "forward10Days": "{{x.buying.forward10Days}}",
+    |        "forward30Days": "{{x.buying.forward30Days}}",
+    |        "forward60Days": "{{x.buying.forward60Days}}",
+    |        "forward90Days": "{{x.buying.forward90Days}}",
+    |        "forward120Days": "{{x.buying.forward120Days}}",
+    |        "forward150Days": "{{x.buying.forward150Days}}"
+    |      },
+    |      "selling": {
+    |        "cash": "{{x.selling.cash}}",
+    |        "spot": "{{x.selling.spot}}",
+    |        "forward10Days": "{{x.selling.forward10Days}}",
+    |        "forward30Days": "{{x.selling.forward30Days}}",
+    |        "forward60Days": "{{x.selling.forward60Days}}",
+    |        "forward90Days": "{{x.selling.forward90Days}}",
+    |        "forward120Days": "{{x.selling.forward120Days}}",
+    |        "forward150Days": "{{x.selling.forward150Days}}"          
+    |      }
+    |    }
 </template>
 
 <style lang="stylus">
 .currency-rate-raw
+  display flex
+  flex-direction column
+  align-items center
+
   &__list
     display block
     margin 1rem 0
